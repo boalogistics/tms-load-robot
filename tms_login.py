00d67a1bd90ceee3f652db6069dae875bbb2a1ee
yhalothar <51811628+yhalothar@users.noreply.github.com>
2020-02-28 18:50:00 -0800

    separated login portion and added message for not booked.

---
+++ tms_login.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def login(url):
+    # activate headless mode
+    options = Options()
+    options.headless = True
+
+    # activate chrome driver
+    browser = webdriver.Chrome(options=options)
+    browser.get(url)
+
+    # page elements to login
+    boa_user = browser.find_element_by_id("txb-username")
+    boa_pw = browser.find_element_by_id("txb-password")
+    login_button = browser.find_element_by_id("ctl00_ContentBody_butLogin")
+
+    # login credentials
+    boa_user.send_keys("***EMAIL HERE***")
+    boa_pw.send_keys("***PASSWORD HERE***")
+    login_button.click()
+    return browser
