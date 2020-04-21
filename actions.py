92fd1ac1d82a57b7f604df6b3494816afca1c40c
yhalothar <51811628+yhalothar@users.noreply.github.com>
2020-04-21 14:00:23 -0700

    comparing old dict method to assign class properties

--- actions.py
+++ actions.py
@@ -20,3 +20,11 @@ class Truck:
         self.carrier = carrier
         self.disaptched = dispatched
 
+
+class Truck:
+    def __init__(self, truck_list):
+        self.load_no = truck_list[0]
+        self.married_load = truck_list[1]
+        self.truck_no = truck_list[2]
+        self.carrier_name = truck_list[3]
+        self.dispatched = truck_list[4]
