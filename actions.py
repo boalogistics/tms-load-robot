4c07e1306dd98f72290756ddca612a459f92cb6b
yhalothar <51811628+yhalothar@users.noreply.github.com>
2020-04-20 17:48:06 -0700

    beginning of Truck class

--- actions.py
+++ actions.py
@@ -10,3 +10,13 @@ def book():
 def dispatch():
     pass
 
+class Truck:
+    """
+    Class to hold all information for the Married / Master truck.
+    Object instance should be the truck number (truck1, truck2, etc.)
+    """
+    def __init__(self, married_load, carrier, dispatched):
+        self.married_load = married_load
+        self.carrier = carrier
+        self.disaptched = dispatched
+
