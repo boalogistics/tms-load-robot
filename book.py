740507260c27c9b64cb8ae7f3ce1fe726d8a581b
Daigo Takamuro <daigo@boalogistics.com>
2020-05-08 18:47:24 -0700

    reordered trace action, replace book click with javascript exec, no more popup

--- book.py
+++ book.py
@@ -15,7 +15,7 @@ url = 'https://boa.3plsystemscloud.com/'
 browser = tms.login(url)
 
 
-load_list = ['158904', '158906', '158907', '158908', '158909', '158910', '158922', '158926', '158930', '158923', '158927', '158931', '158921', '158925', '158929', '158920', '158924', '158928', '158932', '158936', '158940', '158933', '158937', '158935', '158939', '158934', '158938', '158942', '158946', '158943', '158947', '158941', '158945', '158944', '158948', '158968', '158972', '158976', '158980', '158969', '158973', '158977', '158981', '158971', '158975', '158979', '158970', '158974', '158978', '158982', '158986', '158990', '158983', '158987', '158991', '158985', '158989', '158984', '158988', '158992', '158996', '159000', '159004', '158993', '158997', '159001', '159005', '158995', '158999', '159003', '159007', '158994', '158998', '159002', '159006', '159012', '159013']
+load_list = ['154356', '154501']
 
 for x in load_list:
     load_id = x
@@ -30,45 +30,34 @@ for x in load_list:
         client_name = browser.find_element_by_xpath("//div[@id='ctl00_BodyContent_divCustomerInfo']/div[1]/a").text
         logging.info(load_id + ' not booked. Client {} has exceeded credit limit.'.format(client_name))
         loads_not_booked += 1
-    else:
+    else:      
         # check if load is already in booked/dispatched/cancelled status and trace priority
         status = browser.find_element_by_id('lblTitle').text.upper()
-        
-        not_trace_priority = status.find('TRACE') == -1  
-        
-        if not_trace_priority:
-            # set first window handle
-            og_window = browser.window_handles[0]
-            
-            # open "Edit Shipment" window
-            editShipment = browser.find_element_by_link_text('Edit Shipment')
-            editShipment.click()
-            WebDriverWait(browser, timeout=30).until(EC.number_of_windows_to_be(2))
 
-            # set handle to popup and switches to popup
-            popup = browser.window_handles[1]
-            browser.switch_to.window(popup)
-            
-            # variable and selections for Equipment Types
-            priority = Select(browser.find_element_by_id('ctl00_BodyContent_priority'))
-            priority.select_by_value('6')
-            
-            # save changes
-            updButton = browser.find_element_by_css_selector('[value="Update"]')
-            updButton.click()
-                
-            browser.switch_to.window(og_window)
-        
         quote_status = status.find('QUOTED') > -1   
         
         if quote_status:
-            bookshipment = browser.find_element_by_id('ctl00_BodyContent_spnBookShipment')
-            bookshipment.click()
+            js_book = 'WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions("ctl00$BodyContent$lbBookShipment", "", true, "", "", false, true))'
+            browser.execute_script(js_book)
             logging.info(load_id + ' booked.')
             loads_booked += 1
         else:
             logging.info(load_id + ' not booked. ' + status)
             loads_not_booked += 1
+        
+        not_trace_priority = status.find('TRACE') == -1  
+        
+        if not_trace_priority:
+            edit_shipment = 'http://boa.3plsystemscloud.com/App_BW/staff/shipment/EditShipmentPop.aspx?loadid='+load_id
+            browser.get(edit_shipment)
+
+            # variable and selections for Equipment Types
+            priority = Select(browser.find_element_by_id('ctl00_BodyContent_priority'))
+            priority.select_by_value('6')
+            
+            # save changes
+            update_btn = browser.find_element_by_css_selector('[value="Update"]')
+            update_btn.click()
 
 browser.quit()
 
