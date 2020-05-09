8e27e10ed677ae217628b47b5cda12439ce42dd2
Daigo Takamuro <daigo@boalogistics.com>
2020-05-08 18:47:24 -0700

    reordered trace action, replace book click with javascript exec, no more popup

--- checkandbook.py
+++ checkandbook.py
@@ -93,45 +93,34 @@ for x in load_list:
         client_name = browser.find_element_by_xpath("//div[@id='ctl00_BodyContent_divCustomerInfo']/div[1]/a").text
         logging.info(load_id + ' not booked. Client {} has exceeded credit limit.'.format(client_name))
         loads_not_booked += 1
-    else:
+    else:      
         # check if load is already in booked/dispatched/cancelled status and trace priority
         status = browser.find_element_by_id('lblTitle').text.upper()
-        
-        not_trace_priority = status.find('TRACE') == -1  
-        
-        if not_trace_priority:
-            # set first window handle
-            og_window = browser.window_handles[0]
-            
-            # open "Edit Shipment" window
-            editShipment = browser.find_element_by_link_text('Edit Shipment')
-            editShipment.click()
-            WebDriverWait(browser, timeout=30).until(EC.number_of_windows_to_be(2))
 
-            # set handle to popup and switches to popup
-            popup = browser.window_handles[1]
-            browser.switch_to.window(popup)
-            
-            # variable and selections for Equipment Types
-            priority = Select(browser.find_element_by_id('ctl00_BodyContent_priority'))
-            priority.select_by_value('6')
-            
-            # save changes
-            updButton = browser.find_element_by_css_selector('[value="Update"]')
-            updButton.click()
-                
-            browser.switch_to.window(og_window)
-        
         quote_status = status.find('QUOTED') > -1   
         
         if quote_status:
-            bookshipment = browser.find_element_by_id('ctl00_BodyContent_spnBookShipment')
-            bookshipment.click()
+            js_book = 'WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions("ctl00$BodyContent$lbBookShipment", "", true, "", "", false, true))'
+            browser.execute_script(js_book)
             logging.info(load_id + ' booked.')
             loads_booked += 1
         else:
             logging.info(load_id + ' not booked. ' + status)
             loads_not_booked += 1
+        
+        not_trace_priority = status.find('TRACE') == -1  
+        
+        if not_trace_priority:
+            edit_shipment = 'http://boa.3plsystemscloud.com/App_BW/staff/shipment/EditShipmentPop.aspx?loadid='+load_id
+            browser.get(edit_shipment)
+
+            # variable and selections for Equipment Types
+            priority = Select(browser.find_element_by_id('ctl00_BodyContent_priority'))
+            priority.select_by_value('6')
+            
+            # save changes
+            update_btn = browser.find_element_by_css_selector('[value="Update"]')
+            update_btn.click()
 
 browser.quit()
 
