775dc3a634d5a03999f5fe20b7e178f574d613a1
Daigo Takamuro <daigo@boalogistics.com>
2020-05-12 20:49:14 -0700

    searches for index of Discount label, gets discount input box in the next TD element

--- pricer.py
+++ pricer.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def costco_discount(city, pallets, retail):
+    # subtract 1 from pallets argument for list index
     discount_rate = discount_dict[city][pallets - 1]
     # round to nearest multiple of 5
     reduction_value = 5 * round(retail * discount_rate) / 5
@@ -18,56 +19,63 @@ discount_dict = {"West Palm Beach": [0.0294, 0.0466, 0.0542, 0.0601, 0.0593, 0.0
 url = 'https://boa.3plsystemscloud.com/'
 browser = tms.login(url, False)
 
-# put FOR loop here to loop through list of load numbers
-loadlist = ['154356']
+loadlist = ['155819',
+'156269',
+'157825',
+'155884',
+'156162'
+]
 
 for x in loadlist:
-    try:
-        
+    try:        
         load_id = x
         load_url = 'https://boa.3plsystemscloud.com/App_BW/staff/shipment/shipmentDetail.aspx?loadid='+load_id
         browser.get(load_url)
 
         consignee_name = browser.find_element_by_xpath("//div[@id='ctl00_BodyContent_divShipRecLocations']/span/span[2]/div[@class='DivPanelB']/div[@class='PanelBodyForm']/table/tbody/tr[1]/td").text.upper()
-        consignee_city = browser.find_element_by_xpath("//div[@id='ctl00_BodyContent_divShipRecLocations']/span/span[2]/div[@class='DivPanelB']/div[@class='PanelBodyForm']/table/tbody/tr[2]/td").text
+        consignee_city = browser.find_element_by_xpath("//div[@id='ctl00_BodyContent_divShipRecLocations']/span/span[2]/div[@class='DivPanelB']/div[@class='PanelBodyForm']/table/tbody/tr[3]/td").text
         is_costco = consignee_name.find('COSTCO') != -1 and consignee_city in discount_dict
         pallets = 'getpalletcount'
 
         # add pallet counter here
         # how to account for non discount costco locations?
+        # run a report for costco?
 
         edit_pricing = 'http://boa.3plsystemscloud.com/App_BW/staff/shipment/shipmentCostPop.aspx?loadid='+load_id
         browser.get(edit_pricing)
 
-        base_retail = browser.find_element_by_id('ctl00_BodyContent_tbxShippingAmtBilled').get_attribute('value')
+        price_table = browser.find_element_by_id('tblPricing')
+        price_elements = price_table.find_elements_by_tag_name('td')
+        discount_exists = any(td.text == 'Discount:' for td in price_elements)
         
+        while discount_exists == False:
+            supplemental_select = Select(browser.find_element_by_id('ddlAddSupplementals'))
+            supplemental_select.select_by_value('55')
+            add_button = browser.find_element_by_id('btnAddSupplemental')
+            add_button.click()
+            discount_exists = any(td.text == 'Discount:' for td in price_elements)
+
+        base_retail = browser.find_element_by_id('ctl00_BodyContent_tbxShippingAmtBilled').get_attribute('value')
+
         if is_costco:
-            discount = costco_discount(consignee_city, pallets, base_retail)
+            discount = 0
+            # discount = costco_discount(consignee_city, pallets, base_retail)
         else:
             discount = str(float(base_retail) * -0.0415)
 
-        supplemental_select = Select(browser.find_element_by_id('ddlAddSupplementals'))
-        supplemental_select.select_by_value('55')
+        discount_box_pos = [td.text for td in price_elements].index('Discount:')
+        # input box in next TD cell after Discount label
+        discount_cell = price_elements[discount_box_pos + 1]
+        discount_input = discount_cell.find_element_by_tag_name('input')
+        discount_input.send_keys(Keys.CONTROL + 'a')
+        discount_input.send_keys(Keys.DELETE)
+        discount_input.send_keys(discount)
 
-        add_button = browser.find_element_by_id('btnAddSupplemental')
-        add_button.click()
-
-        WebDriverWait(browser, timeout=120).until(EC.element_to_be_clickable((By.ID, 'tbxAccessorialBilledNet55')))
-        discount_box = browser.find_element_by_id('tbxAccessorialBilledNet55')
-        discount_box.send_keys(Keys.CONTROL + 'a')
-        discount_box.send_keys(Keys.DELETE)
-        discount_box.send_keys(discount)
-
-        # save_button = browser.find_element_by_id('btnUpdateCosts')
-        # save_button.click()
-
-        # browser.switch_to.window(og_window)
-        
-
-        
-
+        save_button = browser.find_element_by_id('btnUpdateCosts')
+        save_button.click()
+        print(load_id + ' discounted by ' + discount)
     except Exception as e:
         print(load_id + ' threw ' + repr(e))
 
-# browser.quit()
-
+browser.quit()
+print('Browser closed.')
