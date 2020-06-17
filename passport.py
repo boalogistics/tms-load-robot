dc8b3931a3977d46a656a3af6e9858d79993e614
Daigo Takamuro <daigo@boalogistics.com>
2020-06-17 14:06:20 -0700

    change hardcoded params to variable

--- passport.py
+++ passport.py
@@ -4,5 +4,5 @@ def run_rates(origin, destination, pallets):
     rate_table = pd.read_excel('db/passport.xlsx')
     df = pd.DataFrame(rate_table)
     df = pd.pivot_table(df, index=['Origin', 'Destination'])
-    retail = df.loc['Kent, WA'].loc['Ankeny, IA'][5]
+    retail = df.loc[origin].loc[destination][pallets]
     return retail
