083ee53c312695b86dbe6cf5c4bf4fa153e8c2ce
Daigo Takamuro <daigo@boalogistics.com>
2020-06-25 20:36:29 -0700

    clean df and close webdriver

--- accessorial_report.py
+++ accessorial_report.py
@@ -25,17 +25,23 @@ reports = {
 }
 
 # List of load numbers to get accessorials for
-load_nos = ['143864', '141442', '140695', '141296', '141207', '141293']
-output = "', '".join(load_nos)
+load_nos = []
+output = "'" + "', '".join(load_nos) + "'"
 
 for key in reports:
+    # list of files before downloading
+    before = os.listdir(DOWNLOAD_FOLDER)
+    
     report_code = reports[key]
     report_url = 'https://boa.3plsystemscloud.com/App_BW/staff/Reports/ReportViewer.aspx?code=' + report_code
     browser.get(report_url)
 
-    load_no_box = browser.find_element_by_xpath("//td[1]/input[@class='filter']")
-    load_no_box.clear()
-    load_no_box.send_keys(output)
+    browser.execute_script("document.getElementById('table-wherevalue').firstElementChild.firstElementChild.value =`" + output + "`;")
+
+    # load_no_box = browser.find_element_by_xpath("//td[1]/input[@class='filter']")
+    # load_no_box.clear()
+    # load_no_box.send_keys(output)
+    
 
     # save & view report, then download
     save_button = browser.find_element_by_id('ctl00_ContentBody_butSaveView')
@@ -44,8 +50,6 @@ for key in reports:
     download = browser.find_element_by_id('ctl00_ContentBody_butExportToExcel')
     download.click()
     time.sleep(3)
-
-    browser.close()
 
     #compares list of files in Downloads folder after downloading file to extract filename
     after = os.listdir(DOWNLOAD_FOLDER)
@@ -65,14 +69,16 @@ for key in reports:
     filepath = DOWNLOAD_FOLDER + "\\" + file_name
     data = pd.read_html(filepath)
     df = data[0]
-    date_range = load_nos[0] + load_nos[-1] 
+    df.drop(len(df.index) - 1, inplace=True)
+    load_range = load_nos[0] + load_nos[-1] 
 
-    filesave = DOWNLOAD_FOLDER + "\\" + key + "_" + date_range + ".csv"
+    filesave = DOWNLOAD_FOLDER + "\\access\\" + key + "_" + load_range + ".csv"
 
     df.to_csv(filesave, index = False)
     print("Saved file " + filesave + "!")
     
-    new_source = DOWNLOAD_FOLDER + "\\" + key + "_" + date_range + ".html"
+    new_source = DOWNLOAD_FOLDER + "\\access\\" + key + "-" + load_range + ".html"
     os.rename(filepath, new_source)
     print("Renamed " + filepath + " to " + new_source + "!")
 
+browser.close()
