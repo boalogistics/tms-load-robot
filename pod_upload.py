e0b9463beee4a8fda7c7834fec7f367d5954691b
yhalothar <51811628+yhalothar@users.noreply.github.com>
2020-06-29 12:49:43 -0700

    style

--- pod_upload.py
+++ pod_upload.py
@@ -1,44 +1,47 @@
-import logging, logging.config
-import tms_login as tms
+import logging
+import logging.config
+import os
 from selenium.webdriver.support.ui import Select
+import tms_login as tms
 
 # initialize logger
 logging.config.fileConfig(fname='logs/cfg/upload.conf')
 logger = logging.getLogger('')
 logging.info('==========')
 
-url = 'https://boa.3plsystemscloud.com/'
-browser = tms.login(url)
+BASE_URL = 'https://boa.3plsystemscloud.com/'
+browser = tms.login(BASE_URL)
 
 load_list = []
+
+prefix = 'ctl00_BodyContent_'
 
 for x in load_list:
     public_pod = False
     load_id = x
-    load_url = 'https://boa.3plsystemscloud.com/App_BW/staff/shipment/shipmentDetail.aspx?loadid='+load_id
+    load_url = f'{BASE_URL}App_BW/staff/shipment/shipmentDetail.aspx?loadid={load_id}'
     browser.get(load_url)
-    client_name = browser.find_element_by_xpath("//div[@id='ctl00_BodyContent_divCustomerInfo']/div[1]/a").text
+    client_name = browser.find_element_by_xpath(f"//div[@id='{prefix}divCustomerInfo']/div[1]/a").text
     if client_name == 'Stir Foods LLC':
         public_pod = True
-    upload = 'http://boa.3plsystemscloud.com/App_BW/staff/utilities/DocStorageAdd.aspx?loadid=' + load_id
+    upload = f'{BASE_URL}App_BW/staff/utilities/DocStorageAdd.aspx?loadid={load_id}'
     browser.get(upload)
 
-    choose_file = browser.find_element_by_id('ctl00_BodyContent_fileUpLoadDoc')
+    choose_file = browser.find_element_by_id(f'{prefix}fileUpLoadDoc')
     try:
-        choose_file.send_keys('S:\\POD Folder\\' + load_id + '.pdf')
+        choose_file.send_keys(f'S:\\POD Folder\\{load_id}.pdf')
         if public_pod:
-            view_permission = Select(browser.find_element_by_id('ctl00_BodyContent_rdoPermissionList'))
+            view_permission = Select(browser.find_element_by_id(f'{prefix}rdoPermissionList'))
             view_permission.select_by_value('1')
             logging.info(load_id + ' POD made public for ' + client_name)
 
-        upload_btn = browser.find_element_by_id('ctl00_BodyContent_btnUploadFile')
+        upload_btn = browser.find_element_by_id(f'{prefix}btnUploadFile')
         upload_btn.click()
-        logging.info('POD for ' + load_id + ' uploaded.')
+        logging.info(f'POD for {load_id} uploaded.')
     except Exception as e:
-        logging.info('POD for ' + load_id + ' not uploaded: ' + repr(e))
+        logging.info(f'POD for {load_id} not uploaded: {repr(e)}')
 
 browser.close()
 print('Browser closed.')
 
-import os
 os.startfile('logs\\upload.log')
