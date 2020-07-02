a9f7da003aaf0323f742a1dac7378b94c79dc2b2
yhalothar <51811628+yhalothar@users.noreply.github.com>
2020-07-01 22:57:56 -0700

    takes single dataframe row as argument

--- passport.py
+++ passport.py
@@ -1,10 +1,16 @@
 import json
 import pandas as pd
 
-def run_rates(origin, destination, pallets, temp):
+
+def get_price(dfRow):
+    origin = dfRow['S/ City'] + ', ' + dfRow['S/ State']
+    destination = dfRow['C/ City'] + ', ' + dfRow['C/ State']
+    temp = dfRow['Equipment']
+    pallets = dfRow['Pallets']
+
     rate_table = pd.read_excel('db/passport.xlsx')
     key = json.load(open('db/equipment.json', 'r'))
     df = pd.DataFrame(rate_table)
     df = pd.pivot_table(df, index=['Origin', 'Temp', 'Destination'])
     retail = df.loc[origin].loc[key[temp]].loc[destination][pallets]
-    return retail
+    return [dfRow['Load #'], retail]
