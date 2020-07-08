5f28b0b6d6eccc98059e085154cc9bf6dc7e4258
yhalothar <51811628+yhalothar@users.noreply.github.com>
2020-07-08 13:59:21 -0700

    PREFIX to constant format

--- pod_upload.py
+++ pod_upload.py
@@ -14,32 +14,30 @@ browser = tms.login(BASE_URL)
 
 load_list = []
 
-prefix = 'ctl00_BodyContent_'
+PREFIX = 'ctl00_BodyContent_'
 
 for x in load_list:
-    public_pod = False
     load_id = x
     try:
         pod_path = f'S:\\POD Folder\\{load_id}.pdf'
         load_url = f'{BASE_URL}App_BW/staff/shipment/shipmentDetail.aspx?loadid={load_id}'
         browser.get(load_url)
-        
-        client_name = browser.find_element_by_xpath(f"//div[@id='{prefix}divCustomerInfo']/div[1]/a").text
-        if client_name == 'Stir Foods LLC':
-            public_pod = True
-        
+
+        client_name = browser.find_element_by_xpath(f"//div[@id='{PREFIX}divCustomerInfo']/div[1]/a").text
+        public_pod = client_name == 'Stir Foods LLC'
+
         upload = f'{BASE_URL}App_BW/staff/utilities/DocStorageAdd.aspx?loadid={load_id}'
         browser.get(upload)
 
-        choose_file = browser.find_element_by_id(f'{prefix}fileUpLoadDoc')
+        choose_file = browser.find_element_by_id(f'{PREFIX}fileUpLoadDoc')
         choose_file.send_keys(pod_path)
-        
+
         if public_pod:
-            view_permission = Select(browser.find_element_by_id(f'{prefix}rdoPermissionList'))
+            view_permission = Select(browser.find_element_by_id(f'{PREFIX}rdoPermissionList'))
             view_permission.select_by_value('1')
             logging.info(f'{load_id} POD made public for {client_name}')
 
-        upload_btn = browser.find_element_by_id(f'{prefix}btnUploadFile')
+        upload_btn = browser.find_element_by_id(f'{PREFIX}btnUploadFile')
         upload_btn.click()
         logging.info(f'POD for {load_id} uploaded.')
     except Exception as e:
