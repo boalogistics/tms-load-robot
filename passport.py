579e9b70753a004bc3b9bbedfdb2eff92ffc5341
yhalothar <51811628+yhalothar@users.noreply.github.com>
2020-07-08 13:58:50 -0700

    style

--- passport.py
+++ passport.py
@@ -2,15 +2,15 @@ import json
 import pandas as pd
 
 
-def get_price(dfRow):
-    origin = dfRow['S/ City'] + ', ' + dfRow['S/ State']
-    destination = dfRow['C/ City'] + ', ' + dfRow['C/ State']
-    temp = dfRow['Equipment']
-    pallets = dfRow['Pallets']
+def get_price(df_row):
+    origin = df_row['S/ City'] + ', ' + df_row['S/ State']
+    destination = df_row['C/ City'] + ', ' + df_row['C/ State']
+    temp = df_row['Equipment']
+    pallets = df_row['Pallets']
 
     rate_table = pd.read_excel('db/passport.xlsx')
     key = json.load(open('db/equipment.json', 'r'))
     df = pd.DataFrame(rate_table)
     df = pd.pivot_table(df, index=['Origin', 'Temp', 'Destination'])
     retail = df.loc[origin].loc[key[temp]].loc[destination][pallets]
-    return [dfRow['Load #'], retail]
+    return [df_row['Load #'], retail]
