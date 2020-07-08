30d56c29c21a2c03ce45a8376753e9a43ac7d836
yhalothar <51811628+yhalothar@users.noreply.github.com>
2020-07-08 14:00:15 -0700

    refactor apply_discount to new get_discount func

--- discount.py
+++ discount.py
@@ -14,20 +14,20 @@ discount_rates = [
     0.006, 0.0075, 0.0035, 0.0035, 0.0040,
     0.0045, 0.005, 0.0055, 0.006, 0.006
     ]
-# look up dfRow in json for Costco location x Pallets
-costco_discount_dict = json.load(open('db/costco_dfRow.json', 'r'))
+# look up df_row in json for Costco location x Pallets
+costco_discount_dict = json.load(open('db/costco_table.json', 'r'))
 
 
-def get_price(dfRow):
-    origin = dfRow['S/ City'] + ', ' + dfRow['S/ State']
-    destination = dfRow['C/ City'] + ', ' + dfRow['C/ State']
-    weight = dfRow['Weight']
-    pallets = dfRow['Pallets']
+def get_price(df_row):
+    origin = df_row['S/ City'] + ', ' + df_row['S/ State']
+    destination = df_row['C/ City'] + ', ' + df_row['C/ State']
+    weight = df_row['Weight']
+    pallets = df_row['Pallets']
 
-    rate_dfRow = pd.read_excel('db/stir.xlsx')
+    rate_df_row = pd.read_excel('db/stir.xlsx')
     key = json.load(open('db/region.json', 'r'))
-    df = pd.DataFrame(rate_dfRow)
-    df = pd.pivot_dfRow(df, index=['Origin', 'Destination'])
+    df = pd.DataFrame(rate_df_row)
+    df = pd.pivot_df_row(df, index=['Origin', 'Destination'])
     base_selling = df.loc[key[origin]].loc[destination][pallets]
 
     wt_per_plt = weight / pallets
@@ -63,15 +63,20 @@ def calc_costco_discount(city, pallets, retail):
     return reduction_value
 
 
-def apply_discount(dfRow, price, WebdriverObject):
+def get_discount(df_row, price):
+    consignee_name = df_row['Consignee'].upper()
+    consignee_city = df_row['C/ City']
+    pallets = df_row['Pallets']
+    is_costco = consignee_name.find('COSTCO') != -1 and consignee_city in costco_discount_dict and pallets < 11
+    if is_costco:
+        discount = calc_costco_discount(consignee_city, pallets, price)
+    else:
+        discount = price * -0.0415
+    return discount
+
+
+def apply_discount(load, discount, WebdriverObject):
     try:
-        load_id = str(dfRow['Load #'])
-
-        # how to deal with multistop?
-
-        edit_pricing = f'http://boa.3plsystemscloud.com/App_BW/staff/shipment/shipmentCostPop.aspx?loadid={load_id}'
-        WebdriverObject.get(edit_pricing)
-        # keep this portion of checking if discount exists
         td_list = WebdriverObject.find_elements_by_tag_name('td')
         discount_exists = any(td.text == 'Discount:' for td in td_list)
         while not discount_exists:
@@ -79,31 +84,18 @@ def apply_discount(dfRow, price, WebdriverObject):
             try:
                 supplemental_select.select_by_value('55')
             except Exception as e:
-                logging.exception(f'{load_id} threw {repr(e)}')
+                logging.exception(f'{load} threw {repr(e)}')
             add_button = WebdriverObject.find_element_by_id('btnAddSupplemental')
             add_button.click()
             td_list = WebdriverObject.find_elements_by_tag_name('td')
             discount_exists = any(td.text == 'Discount:' for td in td_list)
-        #move costco check to new get_discount func
-        consignee_name = dfRow['Consignee'].upper()
-        consignee_city = dfRow['C/ City']
-        pallets = dfRow['Pallets']
-        is_costco = consignee_name.find('COSTCO') != -1 and consignee_city in costco_discount_dict and pallets < 11
-        if is_costco:
-            discount = str(calc_costco_discount(consignee_city, pallets, price))
-        else:
-            discount = str(price * -0.0415)
-        # keep the entering discount portion
+
         discount_box_pos = [td.text for td in td_list].index('Discount:')
         # input box in next TD cell after Discount label
         discount_cell = td_list[discount_box_pos + 1]
         discount_input = discount_cell.find_element_by_tag_name('input')
         discount_input.send_keys(Keys.CONTROL + 'a')
         discount_input.send_keys(Keys.DELETE)
-        discount_input.send_keys(discount)
-        # remove button click
-        save_button = WebdriverObject.find_element_by_id('btnUpdateCosts')
-        save_button.click()
-        logging.info(f'{load_id} base retail {price} discounted {discount} ({float(discount)} / {price})')
+        discount_input.send_keys(str(discount))
     except Exception as e:
-        logging.info(f'{load_id} threw {repr(e)}')
+        logging.info(f'{load} threw {repr(e)}')
