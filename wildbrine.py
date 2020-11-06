fb7bada75d36ddfa31a205d37d46b97bc431bf7b
Daigo Takamuro <daigo@boalogistics.com>
2020-11-05 18:19:26 -0800

    init

---
+++ wildbrine.py
@@ -0,0 +1,17 @@
+import json
+import pandas as pd
+
+
+def get_price(df_row):
+    destination = df_row['C/ City'] + ', ' + df_row['C/ State']
+    pallets = df_row['Pallets']
+
+    rate_table = pd.read_excel('db/wildbrine.xlsx')
+    df = pd.DataFrame(rate_table)
+    df = pd.pivot_table(df, index=['Destination'])
+    print(df_row)
+    print(destination)
+    print(df.loc[destination])
+    print(pallets)
+    retail = df.loc[destination][pallets]
+    return [df_row['Load #'], retail]
