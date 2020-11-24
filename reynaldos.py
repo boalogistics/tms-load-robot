5ec1dc6c32931fe65032f7a84862d326648abb93
Daigo Takamuro <daigo@boalogistics.com>
2020-11-23 16:54:35 -0800

    add reynaldos

---
+++ reynaldos.py
@@ -0,0 +1,14 @@
+import json
+import pandas as pd
+
+
+def get_price(df_row):
+    destination = df_row['C/ City'] + ', ' + df_row['C/ State'] + ' ' + df_row['C/ Zip'].astype(int).astype(str).zfill(5)
+
+    pallets = df_row['Pallets']
+
+    rate_table = pd.read_excel('db/reynaldos.xlsx')
+    df = pd.DataFrame(rate_table)
+    df = pd.pivot_table(df, index=['Destination'])
+    retail = df.loc[destination][pallets]
+    return [df_row['Load #'], retail]
