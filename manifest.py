10c5866ac22dff76bdd5f34ce98e4a9119ca5f52
Daigo Takamuro <daigo@boalogistics.com>
2021-01-29 10:20:25 -0800

    increase sleep to 5

--- manifest.py
+++ manifest.py
@@ -43,7 +43,7 @@ save_report_btn.click()
 browser.implicitly_wait(3)
 download = browser.find_element_by_id('ctl00_ContentBody_butExportToExcel')
 download.click()
-time.sleep(3)
+time.sleep(5)
 browser.quit()
 
 # list of files in Downloads folder after downloading to extract filename
