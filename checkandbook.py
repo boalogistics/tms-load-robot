09ad6aa5fb703db33a88d58a97736068ff6cb9d9
daigo@boa <daigo@boalogistics.com>
2021-06-22 13:35:48 -0700

    temp disable credit block

--- checkandbook.py
+++ checkandbook.py
@@ -142,40 +142,42 @@ for x in load_list_str:
     client_credit = browser.find_element_by_id(f'{PREFIX}ctlWarningsVertical_lblCreditWarnings').text.upper()
     client_credit_exceeded = client_credit.find('EXCEEDED') != -1
 
+    # temporarily remove credit check while Reefer restructres
     if client_credit_exceeded:
         client_name = browser.find_element_by_xpath(f"//div[@id='{PREFIX}divCustomerInfo']/div[1]/a").text
         logging.info(f'{load_id} not booked. Client {client_name} has exceeded credit limit.')
-        loads_not_booked += 1
+        # loads_not_booked += 1
+    # uncomment and indent to re-enable credit check
+    # else:
+    # check if load is already in booked/dispatched/cancelled status and trace priority
+    WebDriverWait(browser, timeout=30).until(EC.presence_of_element_located((By.ID, 'lblTitle')))
+    status = browser.find_element_by_id('lblTitle').text.upper()
+
+    quote_status = status.find('QUOTED') > -1
+
+    if quote_status:
+        js_book = 'WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions("ctl00$BodyContent$lbBookShipment", "", true, "", "", false, true))'
+        browser.execute_script(js_book)
+        logging.info(f'Load {load_id} booked.')
+        loads_booked += 1
     else:
-        # check if load is already in booked/dispatched/cancelled status and trace priority
-        WebDriverWait(browser, timeout=30).until(EC.presence_of_element_located((By.ID, 'lblTitle')))
-        status = browser.find_element_by_id('lblTitle').text.upper()
+        logging.info(f'{load_id} not booked. {status}')
+        loads_not_booked += 1
 
-        quote_status = status.find('QUOTED') > -1
+    trace_priority = status.find('TRACE') > -1
 
-        if quote_status:
-            js_book = 'WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions("ctl00$BodyContent$lbBookShipment", "", true, "", "", false, true))'
-            browser.execute_script(js_book)
-            logging.info(f'Load {load_id} booked.')
-            loads_booked += 1
-        else:
-            logging.info(f'{load_id} not booked. {status}')
-            loads_not_booked += 1
+    if not trace_priority:
+        edit_shipment = f'{url}App_BW/staff/shipment/EditShipmentPop.aspx?loadid={load_id}'
+        browser.get(edit_shipment)
 
-        trace_priority = status.find('TRACE') > -1
+        # variable and selections for Equipment Types
+        WebDriverWait(browser, timeout=30).until(EC.presence_of_element_located((By.ID, f'{PREFIX}priority')))
+        priority = Select(browser.find_element_by_id(f'{PREFIX}priority'))
+        priority.select_by_value('6')
 
-        if not trace_priority:
-            edit_shipment = f'{url}App_BW/staff/shipment/EditShipmentPop.aspx?loadid={load_id}'
-            browser.get(edit_shipment)
-
-            # variable and selections for Equipment Types
-            WebDriverWait(browser, timeout=30).until(EC.presence_of_element_located((By.ID, f'{PREFIX}priority')))
-            priority = Select(browser.find_element_by_id(f'{PREFIX}priority'))
-            priority.select_by_value('6')
-
-            # save changes
-            update_btn = browser.find_element_by_css_selector('[value="Update"]')
-            update_btn.click()
+        # save changes
+        update_btn = browser.find_element_by_css_selector('[value="Update"]')
+        update_btn.click()
 
 browser.quit()
 
